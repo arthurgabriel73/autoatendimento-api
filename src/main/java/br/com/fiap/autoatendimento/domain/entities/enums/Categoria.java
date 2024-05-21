999d6453ea542159bd2190f2935f4338aebec342
Arthur <arthurgabriel73@gmail.com>
2024-05-21 20:29:31 -0300

    feat: Add Categoria enum for managing food categories

---
+++ src/main/java/br/com/fiap/autoatendimento/domain/entities/enums/Categoria.java
@@ -0,0 +1,29 @@
+package br.com.fiap.autoatendimento.domain.entities.enums;
+
+import br.com.fiap.autoatendimento.domain.exceptions.ValidationException;
+
+public enum Categoria {
+  LANCHE("Lanche"),
+  ACOMPANHAMENTO("Acompanhamento"),
+  BEBIDA("Bebida"),
+  SOBREMESA("Sobremesa");
+
+  private final String value;
+
+  private Categoria(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public static Categoria fromValue(String value) {
+    for (Categoria categoria : Categoria.values()) {
+      if (categoria.value.equals(value)) {
+        return categoria;
+      }
+    }
+    throw new ValidationException("Categoria inválida: " + value);
+  }
+}
