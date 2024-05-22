509cd3bdeb8378254504f8edf64227fb71829de9
Arthur <arthurgabriel73@gmail.com>
2024-05-22 07:18:42 -0300

    refactor: Update Produto entity with lazy loading for the imagem field

--- src/main/java/br/com/fiap/autoatendimento/domain/entities/Produto.java
+++ src/main/java/br/com/fiap/autoatendimento/domain/entities/Produto.java
@@ -1,25 +1,39 @@
 package br.com.fiap.autoatendimento.domain.entities;
 
 import br.com.fiap.autoatendimento.domain.entities.enums.Categoria;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Table(name = "produtos")
+@Entity(name = "Produto")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Produto {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String nome;
   private String descricao;
   private double preco;
   private String imagem;
-  private Categoria categoria;
   private boolean disponivel;
 
-  public Produto(String nome, String descricao, double preco, String imagem, Categoria categoria, boolean disponivel) {
-    this.nome = nome;
-    this.descricao = descricao;
-    this.preco = preco;
-    this.imagem = imagem;
-    this.categoria = categoria;
-    this.disponivel = disponivel;
-  }
+  @Column(name = "categoria")
+  @Enumerated(EnumType.STRING)
+  private Categoria categoria;
 
   public boolean isDisponivel() {
     return disponivel;
