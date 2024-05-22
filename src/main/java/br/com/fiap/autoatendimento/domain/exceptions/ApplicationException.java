3f4205a4c191ea582231d9b61b526009d81b9e5e
Arthur <arthurgabriel73@gmail.com>
2024-05-22 08:11:05 -0300

    refactor: Replace ApplicationException with BusinessException in exceptions package

--- src/main/java/br/com/fiap/autoatendimento/domain/exceptions/ApplicationException.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.autoatendimento.domain.exceptions;
-
-public class ApplicationException extends RuntimeException {
-  public ApplicationException(String message) {
-    super(message);
-  }
-}
