c43d2e3316307a0dec529a2fe29ac4e08cef9e19
Arthur <arthurgabriel73@gmail.com>
2024-05-23 17:06:07 -0300

    fix: indentation

--- src/main/java/br/com/fiap/autoatendimento/domain/exceptions/ValidationException.java
+++ src/main/java/br/com/fiap/autoatendimento/domain/exceptions/ValidationException.java
@@ -3,7 +3,7 @@ package br.com.fiap.autoatendimento.domain.exceptions;
 public class ValidationException extends BusinessException {
 
 	public ValidationException(String message) {
-	super(message);
+		super(message);
 	}
   
 }
