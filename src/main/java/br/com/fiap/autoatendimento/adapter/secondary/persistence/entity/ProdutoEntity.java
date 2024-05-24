0f22148abc05b21679c9eba5e2d4c807f6a9d11b
Arthur <arthurgabriel73@gmail.com>
2024-05-23 23:29:42 -0300

    refactor: Add auto-generated ID for ProdutoEntity

--- src/main/java/br/com/fiap/autoatendimento/adapter/secondary/persistence/entity/ProdutoEntity.java
+++ src/main/java/br/com/fiap/autoatendimento/adapter/secondary/persistence/entity/ProdutoEntity.java
@@ -19,6 +19,7 @@ import java.util.List;
 public class ProdutoEntity implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_produto")
     private Integer idProduto;
 
