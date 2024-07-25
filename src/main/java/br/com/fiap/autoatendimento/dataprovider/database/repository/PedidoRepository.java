7fc725ace4d70fbd62041077ac6eb7e824c011f4
Anderson <amurasaki@gmail.com>
2024-07-25 19:12:58 -0300

    Alteração lista de pedidos

--- src/main/java/br/com/fiap/autoatendimento/dataprovider/database/repository/PedidoRepository.java
+++ src/main/java/br/com/fiap/autoatendimento/dataprovider/database/repository/PedidoRepository.java
@@ -2,6 +2,17 @@ package br.com.fiap.autoatendimento.dataprovider.database.repository;
 
 import br.com.fiap.autoatendimento.dataprovider.database.entity.PedidoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<PedidoEntity, Integer> {
+
+    @Query(
+            "select p from PedidoEntity p " +
+                    "where p.statusPedido.idStatusPedido not in (4, 5) " +
+                    "order by p.statusPedido.idStatusPedido desc, p.idPedido asc"
+    )
+    List<PedidoEntity> findAllCustom();
+
 }
