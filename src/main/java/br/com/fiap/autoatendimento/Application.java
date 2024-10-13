08fcc3d25336758d39c37b96d11872c3c89f1c60
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-13 18:12:58 -0300

    feat: adds new dev.properties as secret

--- src/main/java/br/com/fiap/autoatendimento/Application.java
+++ src/main/java/br/com/fiap/autoatendimento/Application.java
@@ -2,8 +2,10 @@ package br.com.fiap.autoatendimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:dev.properties")
 public class Application {
 
 	public static void main(String[] args) {
