a78529428cc609dc099d6e73911d2023a511d53c
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-13 17:27:02 -0300

    feat: AppConfig

---
+++ src/main/java/br/com/fiap/autoatendimento/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package br.com.fiap.autoatendimento.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+}
