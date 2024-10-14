90d1ba1ea97af9f880dd3c7e96d6736c88d64490
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-14 19:26:08 -0300

    feat: adds response message

--- src/main/java/br/com/fiap/autoatendimento/entrypoint/rest/HealthCheckController.java
+++ src/main/java/br/com/fiap/autoatendimento/entrypoint/rest/HealthCheckController.java
@@ -1,13 +1,17 @@
 package br.com.fiap.autoatendimento.entrypoint.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/health-check")
 public class HealthCheckController {
-    
+
+    @Value("${spring.application.name}")
+    private String APP_NAME;
+
     @GetMapping
     public String healthCheck() {
-        return "OK";
+        return ("Application: " + APP_NAME + " is running");
     }
 }
